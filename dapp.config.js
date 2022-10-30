ec9cf07413b5fe4bbad0d9e71dacf0c3ac60648f
metalentco <metalentco777@gmail.com>
2022-10-29 23:56:13 -0500

    finish

--- dapp.config.js
+++ dapp.config.js
@@ -1,10 +1,10 @@
 const config = {
   title: 'BoredApe Dapp',
   description: 'A showcase dapp which is built for you to create your own',
-  contractAddress: '0x22D832716D83f44179E6A82541f9Cf04D4BC2405',
+  contractAddress: '0x94f62A5F63D9957D9867fAc87F8542F6F7bBcA83',
   maxMintAmount: 4,
   presaleMaxMintAmount: 4,
-  price: 0.049
+  price: 0.036
 }
 
 export { config }
