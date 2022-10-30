310cd8803a6bc2b59888ba87d4da2487415b4bc0
metalentco <metalentco777@gmail.com>
2022-10-29 21:37:27 -0500

    as

--- dapp.config.js
+++ dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'Doodlin Town',
   description: 'We are 9999 Doodlins that live in the metaverse',
-  contractAddress: '0xb23d2d6075A42E593c00b644e12e16b8F6EDbaD0',
+  contractAddress: '0x94f62A5F63D9957D9867fAc87F8542F6F7bBcA83',
   maxMintAmount: 4,
   presaleMaxMintAmount: 4,
   price: 0.036
